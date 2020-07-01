02abefc9061e5550dcc153bad8919a434ad807e8
unknown <b31@mylangara.ca>
2020-07-01 15:07:45 -0700

    new

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -53,9 +53,69 @@ var settingsView = app.views.create('#view-profile', {
 
 
 
+/* POPOVER ========================================*/
+// Events
+$$('.popover-links').on('popover:open', function (e) {
+  console.log('About popover open');
+});
+$$('.popover-linkst').on('popover:opened', function (e) {
+  console.log('About popover opened');
+});
+/*Edit Photo Button ===============================*/
+//  to open the toast
 
 
+// Create toast with callback on close
+// var toastWithCallback = app.toast.create({
+//   text: 'Callback on close',
+//   closeButton: true,
+//   on: {
+//     close: function () {
+//       app.dialog.alert('Toast closed');
+//     },
+//   }
+// });
+// document.getElementById('.open-toast-callback').addEventListener('click', function () {
+//   console.log("chlra h!");
+//   toastWithCallback.open();
+// });
 
+// Camera ==============================================================
+document.addEventListener("deviceready", init, false);
+function init() {
 
+	function onSuccess(imageData) {
+		console.log('success');
+		var image = document.getElementById('myImage');
+		image.src = imageData;
+	}
+
+	function onFail(message) {
+		navigator.notification.alert(
+			message, null, "Camera Failure");
+	}	
+
+	//Use from Camera
+	document.querySelector("#takePhoto").addEventListener("touchend", function() {
+		navigator.camera.getPicture(onSuccess, onFail, { 
+			quality: 50,
+			sourceType: Camera.PictureSourceType.CAMERA,
+			destinationType: Camera.DestinationType.FILE_URI
+		});
+
+	});
+
+	//Use from Library
+	document.querySelector("#uploadPhoto").addEventListener("touchend", function() {
+		navigator.camera.getPicture(onSuccess, onFail, { 
+			quality: 50,
+			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+			destinationType: Camera.DestinationType.FILE_URI
+		});
+
+	});
+
+}
+// ======================================================================
 
 
